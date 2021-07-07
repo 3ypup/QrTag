20be89b61ae8a79c8410a358d139a8a5cf28d156
3ypup <posilay.pochashe@gmail.com>
2021-07-07 19:34:56 +0300

    added scan result and back cam prefer

--- public/js/qrreader.js
+++ public/js/qrreader.js
@@ -3,14 +3,16 @@
 var resultContainer = document.getElementById('qr-reader-results');
 var lastResult, countResults = 0;
 
-function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
-    console.log(`Scan result: ${decodedText}`, decodedResult);
+
+
+
+
+const html5QrCode = new Html5Qrcode("qr-reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    /* handle success */console.log(`Scan result: ${decodedText}`, decodedResult);
      document.getElementById("demo").innerHTML = decodedText;
+};
+const config = { fps: 60, qrbox: 250 };
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
 
-}
-
-
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
+html5Qrcode.render(onScanSuccess);
