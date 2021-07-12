137b990e3b7f051c4a8fc2f2e92a3896b859e392
3ypup <posilay.pochashe@gmail.com>
2021-07-12 09:29:31 +0300

    background added

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-rails s -b "ssl://0.0.0.0:3030?key=/home/kura/.ssh/server.key&cert=/home/kura/.ssh/server.crt"
+rails s -b "ssl://0.0.0.0:3030?key=/home/amber/.ssh/server.key&cert=/home/amber/.ssh/server.crt"
 
