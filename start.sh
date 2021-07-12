8fcf5e658d9e636e59adfeb473e564f04ae14f1e
3ypup <posilay.pochashe@gmail.com>
2021-07-12 12:28:23 +0300

    devise added

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 clear
-rails s -b "ssl://0.0.0.0:3030?key=/home/amber/.ssh/server.key&cert=/home/amber/.ssh/server.crt"
+#rails s -b "ssl://0.0.0.0:3030?key=/home/amber/.ssh/server.key&cert=/home/amber/.ssh/server.crt"
 
+rails s -b "ssl://0.0.0.0:3000?key=/home/amber/.ssh/server.key&cert=/home/amber/.ssh/server.crt"
